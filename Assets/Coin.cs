40c1e3db8d55e0140245a6792036468a16b38777
Tomislav Eric <hi@tomislav.mobi>
2022-02-16 13:55:13 +0100

    [Day 1] create coin script

---
+++ Assets/Coin.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        spriteRenderer.sprite = Resources.Load<Sprite>("arconsis-logo");
+
+        gameObject.transform.position = new Vector3(14, 5, 0);
+        gameObject.AddComponent<BoxCollider2D>().size = new Vector2(3,3);
+    }
+}
