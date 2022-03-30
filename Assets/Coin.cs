ce43c82fda61cca2e7a0fd6194954417e3ed7e1e
Tomislav Eric <hi@tomislav.mobi>
2022-03-30 11:22:05 +0200

    [DAY 3] set collider size to sprite size

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -6,13 +6,13 @@ public class Coin : MonoBehaviour
 {
     void Start()
     {
-        gameObject.transform.position = new Vector3(14, -2.5f, 0);
-
         var spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
         spriteRenderer.sprite = Resources.Load<Sprite>("arconsis-logo");
 
         var boxCollider = gameObject.GetComponent<BoxCollider2D>();
-        boxCollider.size = new Vector2(3,3);
+
+        boxCollider.size = spriteRenderer.size;
+        boxCollider.offset = spriteRenderer.sprite.bounds.center;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
