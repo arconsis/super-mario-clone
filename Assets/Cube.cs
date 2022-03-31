22acaf4910c441bcea73622dddfcd4a82a2824d5
Tomislav Eric <hi@tomislav.mobi>
2022-03-31 09:03:33 +0200

    [DAY 4] create cube item

---
+++ Assets/Cube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Cube : MonoBehaviour
+{
+    // Use this for initialization
+    void Awake()
+    {
+        var spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        spriteRenderer.sprite = Resources.Load<Sprite>("cube");
+
+        var boxCollider = gameObject.GetComponent<BoxCollider2D>();
+
+        boxCollider.size = spriteRenderer.size;
+        boxCollider.offset = spriteRenderer.sprite.bounds.center;
+    }
+}
